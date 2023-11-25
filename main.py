43ac4d0c09e5cd2eeee403d4a9d3a7049b32c93b
marina <100472115@alumnos.uc3m.es>
2023-11-25 12:53:20 +0100

    Ej 1 parte 2

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+import random
+
 from constraint import *
 import sys
 from vehiculos import Vehiculos
+
+global ad
+global filas
+global columnas
+ad = False
 
 
 def main(file):
     with open(file, "r") as f:
         lectura = f.readlines()
 
-    filas = int(lectura[0][0])
-    columnas = int(lectura[0][2])
+    set_filas(int(lectura[0][0]))
+    set_columnas(int(lectura[0][2]))
 
     dominio_entero = calc_dominio_entero(columnas, filas)
     dominio_electrico = calc_dominio_electrico(lectura[1])
@@ -24,16 +31,81 @@ def main(file):
 
     problem.addConstraint(AllDifferentConstraint())
     for i in variables:
+        set_ad(False)
         for j in variables:
             if i != j:
                 if i.type == "TSU" and j.type == "TNU":
                     problem.addConstraint(prioridad, (i.calc_v(), j.calc_v()))
-    print(len(problem.getSolutions()))
+                problem.addConstraint(adyacencia, (i.calc_v(), j.calc_v()))
+
+    sol = problem.getSolutions()
+    sols = []
+    for i in range(10):
+        num = random.randint(0, len(sol))
+        sols.append(sol[num])
+
+    ind = file.index(".")
+    escr_salida(len(sol), sols, file[0:ind])
+
+
+def escr_salida(num_sol, sols, name):
+    keys = sols[0].keys()
+    with open(name + ".csv", "w") as salida:
+        salida.write("\"N. Sol:\"," + str(num_sol) + "\n")
+        for sol in sols:
+            matriz = []
+            for f in range(filas):
+                row = []
+                for c in range(columnas):
+                    row.append("-")
+                matriz.append(row)
+            for k in keys:
+                matriz[sol[k][0] - 1][sol[k][1] - 1] = k
+            for row in range(len(matriz)):
+                for col in range(len(matriz[row])):
+                    salida.write("\"" + matriz[row][col] + "\"")
+                    if col < len(matriz[row]) - 1:
+                        salida.write(",")
+                    else:
+                        salida.write("\n")
+            salida.write("\n")
+
+
+def set_ad(value):
+    global ad
+    ad = value
+
+
+def set_filas(value):
+    global filas
+    filas = value
+
+
+def set_columnas(value):
+    global columnas
+    columnas = value
+
+
+def adyacencia(vehiculo, vadj):
+    if vehiculo[1] == vadj[1]:
+        # Esta arriba
+        if vehiculo[0] == 1:
+            return vadj[0] != 2
+        # Esta abajo
+        elif vehiculo[0] == filas:
+            return vadj[0] != (filas - 1)
+        else:
+            if ad == False and (vadj[0] == vehiculo[0] - 1 or vadj[0] == vehiculo[0] + 1):
+                set_ad(True)
+                return True
+            else:
+                return vadj[0] != vehiculo[0] - 1 and vadj[0] != vehiculo[0] + 1
+    return True
 
 
 def prioridad(vehiculo_i, vehiculo_j):
     if vehiculo_i[0] == vehiculo_j[0]:
-        return vehiculo_i[1] > vehiculo_j[0]
+        return vehiculo_i[1] > vehiculo_j[1]
     return True
 
 
@@ -68,9 +140,9 @@ def calc_dominio_electrico(lista_elec):
 
 def calc_dominio_entero(columnas, filas):
     dominio_entero = []
-    for col in range(columnas):
-        for row in range(filas):
-            pos = (row+ 1, col + 1)
+    for row in range(filas):
+        for col in range(columnas):
+            pos = (row + 1, col + 1)
             dominio_entero.append(pos)
     return dominio_entero
 
